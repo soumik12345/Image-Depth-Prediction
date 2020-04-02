4df32cbfef45371ab9c6f90d44fa7f26ef816c34
soumik12345 <19soumik.rakshit96@gmail.com>
2020-04-02 17:38:59 +0530

    Adde Generator

---
+++ models.md
@@ -0,0 +1,5 @@
+# Models
+
+## Generator
+
+![](./assets/generator.png)
--- src/blocks.py
+++ src/blocks.py
@@ -3,23 +3,23 @@ import tensorflow as tf
 
 def downsample_block(filters, kernel_size, batch_norm = True):
     initializer = tf.random_normal_initializer(0.0, 0.02)
-    block = tf.keras.layers.Sequential()
+    block = tf.keras.Sequential()
     block.add(
-        tf.keas.layers.Conv2D(
+        tf.keras.layers.Conv2D(
             filters, kernel_size,
             strides=2, padding='same',
             kernel_initializer=initializer, use_bias=False
         )
     )
     if batch_norm:
-        block.add(tf.keas.layers.BatchNormalization())
-    block.add(tf.keas.layers.LeakyReLU())
+        block.add(tf.keras.layers.BatchNormalization())
+    block.add(tf.keras.layers.LeakyReLU())
     return block
 
 
 def upsample_block(filters, kernel_size, dropout = False):
     initializer = tf.random_normal_initializer(0.0, 0.02)
-    block = tf.keras.layers.Sequential()
+    block = tf.keras.Sequential()
     block.add(
         tf.keras.layers.Conv2DTranspose(
             filters, kernel_size,
